efd1df0a5795facce1d9410242bd83c27d127363
Rajesh Routray <rajeshroutray22@gmail.com>
2021-04-24 00:04:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const initRoutes = require("./js/web");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
